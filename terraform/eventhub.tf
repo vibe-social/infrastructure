eb06051c84b3f8d4920ab845561f8a2d37a6b02e
Rok Mokotar <mokotar.rok@gmail.com>
2023-12-27 20:02:14 +0100

    Add event hub and data storage to infra

---
+++ terraform/eventhub.tf
@@ -0,0 +1,42 @@
+resource "azurerm_eventhub_namespace" "eventhub_namespace" {
+  name                = "${var.project_name}-eventhub"
+  location            = azurerm_resource_group.resource_group.location
+  resource_group_name = azurerm_resource_group.resource_group.name
+  sku                 = "Standard"
+  capacity            = 1
+
+  tags = {
+    environment = var.env_name
+  }
+}
+
+resource "azurerm_eventhub" "eventhub" {
+  name                = "${var.project_name}-eventhub"
+  namespace_name      = azurerm_eventhub_namespace.eventhub_namespace.name
+  resource_group_name = azurerm_resource_group.resource_group.name
+  partition_count     = 2
+  message_retention   = 1
+
+  capture_description {
+    enabled             = true
+    encoding            = "Avro"
+    interval_in_seconds = 120
+    size_limit_in_bytes = 10485760
+
+    destination {
+      name                = "EventHubArchive.AzureBlockBlob"
+      archive_name_format = "{Namespace}/{EventHub}/{PartitionId}/{Year}/{Month}/{Day}/{Hour}/{Minute}/{Second}"
+      blob_container_name = "eventhub-archive"
+      storage_account_id  = azurerm_storage_account.storage_account.id
+    }
+  }
+}
+
+resource "azurerm_eventhub_namespace_authorization_rule" "eventhub_namespace_authorization_rule" {
+  name                = "${var.project_name}-eventhub-authorization-rule"
+  namespace_name      = azurerm_eventhub_namespace.eventhub_namespace.name
+  resource_group_name = azurerm_resource_group.resource_group.name
+  listen              = true
+  send                = true
+  manage              = false
+}
---
+++ terraform/storage.tf
@@ -0,0 +1,17 @@
+resource "azurerm_storage_account" "storage_account" {
+  name                     = "standardlrsaccount"
+  resource_group_name      = azurerm_resource_group.resource_group.name
+  location                 = azurerm_resource_group.resource_group.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  tags = {
+    environment = var.env_name
+  }
+}
+
+resource "azurerm_storage_container" "storage_container" {
+  name                  = "${var.project_name}-eventhub-archive"
+  storage_account_name  = azurerm_storage_account.storage_account.name
+  container_access_type = "private"
+}
