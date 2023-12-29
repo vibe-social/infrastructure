2a71b1750bf61af0d8311ab1a4c0873c98155b84
Rok Mokotar <mokotar.rok@gmail.com>
2023-12-29 13:51:50 +0100

    Increase versions

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.0.2"
+      version = "~> 3.85.0"
     }
   }
 
-  required_version = ">= 1.1.0"
+  required_version = ">= 1.6.6"
 }
 
 provider "azurerm" {
