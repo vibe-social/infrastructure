5efc735d1b09775aa94f74b51c8a65b63f716de1
Rok Mokotar <mokotar.rok@gmail.com>
2023-12-29 19:50:56 +0100

    Add new authorizatoin rule

--- terraform/eventhub.tf
+++ terraform/eventhub.tf
@@ -32,8 +32,18 @@ resource "azurerm_eventhub" "eventhub" {
   }
 }
 
-resource "azurerm_eventhub_namespace_authorization_rule" "eventhub_namespace_authorization_rule" {
+resource "azurerm_eventhub_authorization_rule" "eventhub_authorization_rule" {
   name                = "${var.project_name}-eventhub-authorization-rule"
+  namespace_name      = azurerm_eventhub_namespace.eventhub_namespace.name
+  resource_group_name = azurerm_resource_group.resource_group.name
+  eventhub_name       = azurerm_eventhub.eventhub.name
+  listen              = true
+  send                = true
+  manage              = false
+}
+
+resource "azurerm_eventhub_namespace_authorization_rule" "eventhub_namespace_authorization_rule" {
+  name                = "${var.project_name}-eventhub-namespace-authorization-rule"
   namespace_name      = azurerm_eventhub_namespace.eventhub_namespace.name
   resource_group_name = azurerm_resource_group.resource_group.name
   listen              = true
